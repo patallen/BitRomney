a4c81dc01febc2cae1512f9f337013dd1afd3e67
Patrick Allen <prallen90@gmail.com>
2017-04-06 01:33:23 -0400

    Add a bunch of opcodes - listed below
    - 0x0006
    - 0x0011
    - 0x001A
    - 0x004F
    - 0x0077
    - 0x00C5
    - 0x00CD
    - 0x00E0
    - 0xCB11

--- src/operations.rs
+++ src/operations.rs
@@ -43,7 +43,13 @@ pub fn get_operation(code: u16) -> Operation {
     match prefix {
         0x00 => match scode {   // No Prefix
             0x00 => match lcode {
+                0x06 => Operation::new(code, opx06, 2, 8,  "LD B, d8"),
                 0x0E => Operation::new(code, opx0E, 2, 8,  "LD C, d8"),
+                _   => Operation::new(code, unimplemented, 0, 0, "unimplemented"),
+            },
+            0x10 => match lcode {
+                0x01 => Operation::new(code, opx11, 3, 12,  "LD DE, d16"),
+                0x0A => Operation::new(code, opx1A, 1, 8,  "LD A, (DE)"),
                 _   => Operation::new(code, unimplemented, 0, 0, "unimplemented"),
             },
             0x20 => match lcode {
@@ -57,20 +63,35 @@ pub fn get_operation(code: u16) -> Operation {
                 0x0E => Operation::new(code, opx3E, 2, 8,  "LD A, d8"),
                 _   => Operation::new(code, unimplemented, 0, 0, "unimplemented"),
             },
+            0x40 => match lcode {
+                0x0F => Operation::new(code, opx4F, 1, 4,  "LD C, A"),
+                _   => Operation::new(code, unimplemented, 0, 0, "unimplemented"),
+            },
             0x70 => match lcode {
+                0x07 => Operation::new(code, opx77, 1, 8, "LD (HL), A"),
                 _   => Operation::new(code, unimplemented, 0, 0, "unimplemented"),
             },
             0xA0 => match lcode {
                 0x0F => Operation::new(code, opxAF, 1, 4, "XOR A, A"),
                 _   => Operation::new(code, unimplemented, 0, 0, "unimplemented"),
             },
+            0xC0 => match lcode {
+                0x05 => Operation::new(code, opxC5, 1, 16, "PUSH BC"),
+                0x0D => Operation::new(code, opxCD, 3, 24, "CALL a16"),
+                _   => Operation::new(code, unimplemented, 0, 0, "unimplemented"),
+            },
             0xE0 => match lcode {
+                0x00 => Operation::new(code, opxE0, 2, 12, "LDH (a8), A"),
                 0x02 => Operation::new(code, opxE2, 2, 8, "LD (C), A"),
                 _   => Operation::new(code, unimplemented, 0, 0, "unimplemented"),
             },
             _   => Operation::new(code, unimplemented, 0, 0, "unimplemented"),
         },
         0xCB => match scode {   // CB Prefix
+            0x10 => match lcode {
+                0x01 => Operation::new(code, cbx11, 2, 8, "RL C"),
+                _   => Operation::new(code, unimplemented, 0, 0, "unimplemented"),
+            },
             0x70 => match lcode {
                 0x0C => Operation::new(code, cbx7C, 2, 8, "BIT 7, H"),
                 _   => Operation::new(code, unimplemented, 0, 0, "unimplemented"),
@@ -116,34 +137,95 @@ pub fn opxAF(cpu: &mut Cpu, mmu: &mut Mmu) {
     let new = cpu.regs.get_u16(Reg::AF) & 0xFF00;
     cpu.regs.set_u16(Reg::AF, new);
 }
+pub fn opx0E(cpu: &mut Cpu, mmu: &mut Mmu) {
+    let next = cpu.immediate_u8(mmu);
+    cpu.regs.set_u8(Reg::C, next);
+}
+pub fn opx3E(cpu: &mut Cpu, mmu: &mut Mmu) {
+    let val = cpu.immediate_u8(mmu);
+    cpu.regs.set_u8(Reg::A, val);
+}
+pub fn opxE2(cpu: &mut Cpu, mmu: &mut Mmu) {
+    let c = cpu.regs.get_u8(Reg::C);
+    let a = cpu.regs.get_u8(Reg::A);
+    mmu.write(c as usize, a);
+}
+pub fn gload(cpu: &mut Cpu, mmu: &mut Mmu) {
+
+}
+pub fn opx77(cpu: &mut Cpu, mmu: &mut Mmu) {
+    // LD (HL), A
+    let hl = cpu.regs.get_u16(Reg::HL) as usize;
+    let a = cpu.regs.get_u8(Reg::A);
+    mmu.write(hl, a);
+}
+pub fn opxE0(cpu: &mut Cpu, mmu: &mut Mmu) {
+    let addr = (0xFF00 + cpu.immediate_u8(mmu) as u16) as usize;
+    mmu.write(addr, cpu.regs.get_u8(Reg::A));
+}
+pub fn opx11(cpu: &mut Cpu, mmu: &mut Mmu) {
+    // LD DE, d16
+    let d16 = cpu.immediate_u16(mmu);
+    cpu.regs.set_u16(Reg::DE, d16);
+}
+pub fn opx1A(cpu: &mut Cpu, mmu: &mut Mmu) {
+    // "LD A, (DE)"
+    let addr = cpu.regs.get_u16(Reg::DE) as u8;
+    println!("value: {:04X}", addr);
+    cpu.regs.set_u8(Reg::A, addr);
+}
+
+pub fn opxCD(cpu: &mut Cpu, mmu: &mut Mmu) {
+    // Call a16
+    let pc = cpu.pc;
+    let nn = cpu.immediate_u16(mmu);
+    mmu.write(cpu.regs.get_u16(Reg::SP) as usize, (pc & 0x00FF) as u8);
+    cpu.regs.inc_sp();
+    mmu.write(cpu.regs.get_u16(Reg::SP) as usize, (pc >> 8) as u8);
+    cpu.regs.inc_sp();
+    cpu.pc = (nn as usize) - 3;
+}
+
+pub fn opx4F(cpu: &mut Cpu, mmu: &mut Mmu) {
+    // LD C, A
+    let a = cpu.regs.get_u8(Reg::A);
+    cpu.regs.set_u8(Reg::C, a);
+}
+pub fn opx06(cpu: &mut Cpu, mmu: &mut Mmu) {
+    // LD B, d8
+    let d8 = cpu.immediate_u8(mmu);
+    cpu.regs.set_u8(Reg::B, d8);
+}
+
+pub fn opxC5(cpu: &mut Cpu, mmu: &mut Mmu) {
+    // PUSH BC
+    let bc = cpu.regs.get_u16(Reg::BC);
+    mmu.write(cpu.regs.get_u16(Reg::SP) as usize, bc.get_msb());
+    cpu.regs.inc_sp();
+    mmu.write(cpu.regs.get_u16(Reg::SP) as usize, bc.get_lsb());
+    cpu.regs.inc_sp();
+}
 
 pub  fn cbx7C(cpu: &mut Cpu, mmu: &mut Mmu) {
-    // Test bit 7 in register H & if set, set z to 1
-    // always set flags N=0 and H=1
     let mut flags = cpu.flags();
     let hbit = cpu.regs.get_u8(Reg::H).get_bit(7);
     flags.set_bit(flag::N as usize, hbit);
     cpu.regs.set_u8(Reg::F, flags);
 }
 
-pub fn opx0E(cpu: &mut Cpu, mmu: &mut Mmu) {
-    let next = cpu.immediate_u8(mmu);
-    cpu.regs.set_u8(Reg::C, next);
-}
-
-pub fn opx3E(cpu: &mut Cpu, mmu: &mut Mmu) {
-    // load A, d8
-    let val = cpu.immediate_u8(mmu);
-    cpu.regs.set_u8(Reg::A, val);
-}
-
-pub fn opxE2(cpu: &mut Cpu, mmu: &mut Mmu) {
-    // LD (C), A (LD the value of A into location specified in C)
+pub  fn cbx11(cpu: &mut Cpu, mmu: &mut Mmu) {
+    // RL C
+    // Shift C one bit to the left
+    // bit 0 of C is set to the carry flag
+    // the carry flag is set to the MSB of the original C
+    let mut flags = cpu.flags();
     let c = cpu.regs.get_u8(Reg::C);
-    let a = cpu.regs.get_u8(Reg::A);
-    mmu.write(c as usize, a);
-}
-
-pub fn gload(cpu: &mut Cpu, mmu: &mut Mmu) {
+    let msb = c >> 7;
+    let carry = flags.get_bit(flag::C as usize);
+    let res = (c << 1) & msb;
+    cpu.regs.set_u8(Reg::C, res);
+    flags.set_bit(flag::C, carry);
+    flags.set_bit(flag::Z, (res == 0) as u8);
+    cpu.regs.set_u8(Reg::F, flags);
 
 }
