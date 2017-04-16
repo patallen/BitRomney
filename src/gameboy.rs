545eedf68c05ebd3de2e0731d9edf7bc208efeab
Patrick Allen <prallen90@gmail.com>
2017-04-16 01:12:54 -0400

    Clean up the Display code

--- src/gameboy.rs
+++ src/gameboy.rs
@@ -6,37 +6,55 @@ extern crate sdl2;
 use self::sdl2::render::{Renderer, Texture};
 use self::sdl2::pixels::{Color, PixelFormatEnum};
 
-const DISPLAY_WIDTH: u32 = 320;
-const DISPLAY_HEIGHT: u32 = 288;
-
+const DISPLAY_WIDTH  : u32 = 160;
+const DISPLAY_HEIGHT : u32 = 144;
+const SCALE          : u32 = 2;
+const TITLE          : &'static str = "BitRomney GB";
+const BACKGROUND     : (u8, u8, u8) = (155, 188, 15);
 
 struct Display {
     renderer: Renderer<'static>,
-    texture: Texture
+    texture: Texture,
+    prev_texture: Texture,
 }
 
 impl Display {
     pub fn new(context: sdl2::Sdl) -> Display {
+        let width = DISPLAY_WIDTH;
+        let height = DISPLAY_HEIGHT;
+
         let video = context.video().unwrap();
-        let window = video.window("BitRomney", DISPLAY_WIDTH, DISPLAY_HEIGHT)
-            .position_centered().opengl()
-            .build().unwrap();
+        let window = video.window(TITLE, width * SCALE, height * SCALE)
+                          .position_centered().opengl()
+                          .build().unwrap();
         let mut renderer = window.renderer().accelerated().build().unwrap();
-        renderer.set_draw_color(Color::RGB(123, 123, 123));
+
+        let (r, g, b) = BACKGROUND;
+        renderer.set_draw_color(Color::RGB(r, g, b));
         renderer.clear();
         renderer.present();
-        let texture = renderer.create_texture_streaming(
-            PixelFormatEnum::ARGB8888, DISPLAY_WIDTH, DISPLAY_HEIGHT).unwrap();
+
+        let texture = renderer
+            .create_texture_streaming(PixelFormatEnum::ARGB8888, width, height)
+            .unwrap();
+        let prev_texture = renderer
+            .create_texture_streaming(PixelFormatEnum::ARGB8888, width, height)
+            .unwrap();
         Display {
             renderer: renderer,
             texture: texture,
+            prev_texture: prev_texture,
         }
     }
     pub fn draw_frame(&mut self, data: [u8; 23_040 * 4]){
         self.renderer.clear();
+        self.renderer.copy(&self.prev_texture, None, None).unwrap();
+        self.texture.update(None, &data, DISPLAY_WIDTH as usize * 4).unwrap();
+        self.texture.set_alpha_mod(127);
+        self.texture.set_blend_mode(sdl2::render::BlendMode::Blend);
         self.renderer.copy(&self.texture, None, None).unwrap();
-        self.texture.update(None, &data, 100).unwrap();
         self.renderer.present();
+        self.prev_texture.update(None, &data, DISPLAY_WIDTH as usize * 4).unwrap();
     }
 }
 pub struct Gameboy {
